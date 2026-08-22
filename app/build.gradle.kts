f155abcfa84fa1fbdc25b588b3769119c11c51d9
Turkey <forgejo.shawl406@passmail.net>
2026-08-22 01:50:28 +0000

    v2.2.0: version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.turkbot.babytracker"
         minSdk = 26
         targetSdk = 34
-        versionCode = 48
-        versionName = "2.1.0"
+        versionCode = 49
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
